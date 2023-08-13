ce80cad79263409ccee7eda0e8bd14d43e36c3b8
bharathsaravanan2001 <bharathsaravananofficial@gmail.com>
2023-08-13 19:40:55 +0530

    table added

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,8 +3,8 @@ import Excel from "exceljs";
 
 export default function Home() {
 
-    const [fileData, setFileData] = useState({}); 
-
+    const [fileData, setFileData] = useState([]); 
+    const [tabVisible, setTabVisible] = useState(false);
     const getFile = (e) => {
         const file = e.target.files[0];
         const wb = new Excel.Workbook();
@@ -14,24 +14,54 @@ export default function Home() {
         reader.onload = () => {
             const buffer = reader.result;
             wb.xlsx.load(buffer).then((workbook) => {
-                console.log(workbook, 'workbook instance');
                 workbook.eachSheet((sheet, id) => {
-                    sheet.eachRow((row, rowIndex) => {
-                        console.log(row.values, rowIndex);
-                    });
+                    if (sheet.name == "Athletes") {
+                        sheet.eachRow((row, rowIndex) => {
+                            if (rowIndex > 3) {
+                                    var rowAr = Array.from(row.values);
+                                    setFileData(prev => {
+                                        return [...prev, rowAr]
+                                    })
+                            }
+                        });
+                    }
                 });
-            });
+            })
         };
     }
 
     return (
-        <form>
-            <input 
-                type="file" 
-                id="file" 
-                name="file"
-                onChange={(data) => getFile(data)}
-                 />
-        </form>
+        <React.Fragment>
+            <form>
+                <input 
+                    type="file" 
+                    id="file" 
+                    name="file"
+                    onChange={(data) => getFile(data)}
+                    />
+            </form>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Country</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {fileData && fileData.map((row, rowIndex) => {
+                            if (rowIndex > 3) {
+                                return (
+                                    <tr key={rowIndex}>
+                                        <td>{row[1]}</td>
+                                        <td>{row[3]}</td>
+                                    </tr>
+                                )
+                            }
+
+                        })}
+                    </tbody>
+            </table>
+        </React.Fragment>
+
     )
 }
