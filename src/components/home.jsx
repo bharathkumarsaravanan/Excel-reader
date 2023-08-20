6326b7adcc6078d88a80f91397bdab97bcbdc9dd
bharathsaravanan2001 <bharathsaravananofficial@gmail.com>
2023-08-20 21:57:06 +0530

    test app

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Excel from "exceljs";
+import emailjs from '@emailjs/browser';
 
 export default function Home() {
 
@@ -15,19 +16,41 @@ export default function Home() {
             const buffer = reader.result;
             wb.xlsx.load(buffer).then((workbook) => {
                 workbook.eachSheet((sheet, id) => {
-                    if (sheet.name == "Athletes") {
                         sheet.eachRow((row, rowIndex) => {
-                            if (rowIndex > 3) {
+                            if (rowIndex > 1) {
                                     var rowAr = Array.from(row.values);
                                     setFileData(prev => {
                                         return [...prev, rowAr]
                                     })
                             }
                         });
-                    }
                 });
             })
         };
+    }
+
+    const sendMail = (to) => {
+
+        const user = fileData.find((data, index) => index == to);
+
+        if (user[2] && user[3] && user[12]) {
+            emailjs.send('service_8t1dg84', 
+                        'template_olakybd', 
+                        {
+                            from_name: "cts alert",
+                            to_name: user[2],
+                            message: user[3],
+                            to_mail: user[12],
+                        }, 
+                        '2ivqwepktUEzNazVV')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        }
+
+
     }
 
     return (
@@ -37,26 +60,29 @@ export default function Home() {
                     type="file" 
                     id="file" 
                     name="file"
+                    disabled={fileData.length!=0}
                     onChange={(data) => getFile(data)}
                     />
             </form>
-                <table>
+                <table hidden={!fileData.length!=0}>
                     <thead>
                         <tr>
+                            <th>S.no</th>
                             <th>Name</th>
-                            <th>Country</th>
+                            <th>Email</th>
                         </tr>
                     </thead>
                     <tbody>
                     {fileData && fileData.map((row, rowIndex) => {
-                            if (rowIndex > 3) {
                                 return (
                                     <tr key={rowIndex}>
-                                        <td>{row[1]}</td>
-                                        <td>{row[3]}</td>
+                                        <td>{rowIndex + 1 + " ."}</td>
+                                        <td>{row[2]}</td>
+                                        <td>{row[12]}</td>
+                                        <td><button onClick={() => sendMail(rowIndex)}>Send mail</button></td>
                                     </tr>
                                 )
-                            }
+                            
 
                         })}
                     </tbody>
